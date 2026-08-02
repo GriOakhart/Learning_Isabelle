47c90c238aae11a719cd206a727d9079e6625d0f
GriOakhart <GriOakhart@users.noreply.github.com>
2026-08-02 15:15:48 -0700

    Expand Chapter_1 with bool conj, nat add, and induction.
    Follows sections 2.2.1–2.2.2: define conj and recursive add, evaluate add 2 3, and prove add m 0 = m by induction.

--- Chapter_1.thy
+++ Chapter_1.thy
@@ -2,15 +2,28 @@ theory Chapter_1
   imports Main
 
 begin
-(*
-datatype bool = True | False
-*)
-definition xor :: "bool \<Rightarrow> bool \<Rightarrow> bool" where
-  "xor a b = (a \<noteq> b)"
 
-value "xor True True"
-value "xor True False"
-value "xor False True"
-value "xor False False"
+text \<open>section 2.2.1 Type bool\<close>
+
+(*datatype bool = True | False*)
+
+fun conj :: "bool \<Rightarrow> bool \<Rightarrow> bool" where
+  "conj True True = True"
+| "conj _ _ = False"
+
+text \<open>section 2.2.2 Type nat\<close>
+
+(*datatype nat = 0 | Suc nat*)
+
+fun add :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
+  "add 0 n = n"
+| "add (Suc m) n = Suc (add m n)"
+
+value "add 2 3"
+
+lemma add_02: "add m 0 = m"
+  apply(induction m)
+   apply(auto)
+  done
 
 end
