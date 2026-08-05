765b1504cd2bdc52508480fe7766d356f91bc057
GriOakhart <GriOakhart@users.noreply.github.com>
2026-08-06 01:44:23 +0200

    Add exercises 2.3–2.4: count, snoc, and reverse.
    Implement count_01 with length bound, snoc/reverse with reverse_snoc helper, and prove reverse (reverse xs) = xs.

--- Exercises.thy
+++ Exercises.thy
@@ -50,4 +50,54 @@ lemma double_01: "double m = add m m"
    apply(auto)
   done
 
+text \<open>Exercise 2.3\<close>
+
+fun count_01 :: "'a \<Rightarrow> 'a list \<Rightarrow> nat" where
+  "count_01 m Nil = 0"
+| "count_01 m (x # xs) = (if m = x then Suc (count_01 m xs) else count_01 m xs)"
+  \<comment> \<open>On the right-hand side of a fun equation,
+      bare if \<dots> then \<dots> else \<dots> does not parse.
+      The conditional must be wrapped in parentheses
+      so the whole expression is one term.\<close>
+
+(*
+  INVALID: Isabelle’s fun package requires linear patterns:
+  each variable may occur only once on the left.
+fun count_02 :: "'a \<Rightarrow> 'a list \<Rightarrow> nat" where
+  "count_02 m Nil = 0"
+| "count_02 m (m # xs) = Suc (count_02 m xs)" \<comment> \<open>twice here\<close>
+| "count_02 m (_ # xs) = count_02 m xs"
+*)
+
+lemma count_03: "count_01 m xs \<le> length xs"
+  apply(induction xs)
+   apply(auto)
+  done
+
+text \<open>Exercise 2.4\<close>
+
+(*append an element to the end of the list*)
+fun snoc :: "'a list \<Rightarrow> 'a \<Rightarrow> 'a list" where
+  "snoc Nil m = m # Nil"
+| "snoc (x # xs) m = x # snoc xs m"
+
+value "snoc [(2::nat), 4, 6] 5"
+
+fun reverse :: "'a list \<Rightarrow> 'a list" where
+  "reverse Nil = Nil"
+| "reverse (x # xs) = snoc (reverse xs) x"
+
+value "reverse (''apple''::string)"
+
+(*from the 2nd subgoal in proof of double_reverse,
+  we need a helper lemma of reverse interacting on snoc*)
+lemma reverse_snoc [simp]: "reverse (snoc xs m) = m # reverse xs"
+  apply(induction xs)
+   apply(auto)
+  done
+
+lemma double_reverse: "reverse (reverse xs) = xs"
+  apply(induction xs)
+   apply(auto)
+  done
 end
